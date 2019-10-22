5c9a8525101dfdd4b8b0a3ff6a787732b5cbbb00
Randy LeVeque <rjl@uw.edu>
2019-10-21 17:17:55 -0700

    fix ticklabel_format style param in topotools

--- new_python/topotools.py
+++ new_python/topotools.py
@@ -1117,7 +1117,7 @@ class Topography(object):
             axes = fig.add_subplot(111)
         
         # Turn off annoying offset
-        axes.ticklabel_format(format="plain", useOffset=False)
+        axes.ticklabel_format(style="plain", useOffset=False)
         for label in axes.get_xticklabels():
             label.set_rotation(20)
 
