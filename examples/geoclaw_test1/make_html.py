61e3bed3d9ed5d241cc58f727b73b77de427db37
Randy LeVeque <rjl@uw.edu>
2019-11-10 17:15:37 +0900

    make html files in right order

--- examples/geoclaw_test1/make_html.py
+++ examples/geoclaw_test1/make_html.py
@@ -3,7 +3,9 @@ import sys
 sys.path.insert(0,'../../notebooks')
 import convert_notebooks
 
-all_notebooks = 'all'
+all_notebooks = ['geoclaw_test1_index.ipynb',
+                 'MakeInputFiles_test1.ipynb',
+                 'RunGeoclaw_test1.ipynb']
 run_notebooks = True
 
 convert_notebooks.make_html(all_notebooks,run_notebooks)
